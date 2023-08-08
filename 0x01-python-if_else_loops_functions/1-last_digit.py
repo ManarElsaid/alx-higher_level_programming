7a86055c38d2b5ba65538e89c04b8e132faea1e0
ManarElsaid <elsaidmanar@gmail.com>
2023-08-08 11:59:46 -0700

    dealing with the negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0 and (number % 10 != 0):
-    last_digit = -1 * (number % 10)
+if number < 0:
+    last_digit = number % -10
 else:
     last_digit = number % 10
 if last_digit > 5:
