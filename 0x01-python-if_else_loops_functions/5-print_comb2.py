f9f0e99358c274dd90a0a58962ad272483ed0f90
ManarElsaid <elsaidmanar@gmail.com>
2023-08-09 03:32:14 -0700

    print comb2 in two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(100):
     if n == 99:
-        print("{:2d}".format(n))
+        print("{:02d}".format(n))
     else:
-        print("{:2d}".format(n), end=", ")
+        print("{:02d}".format(n), end=", ")
