7a1b6218e97a8897a33734a7a1c10f909fa7b6c5
ManarElsaid <elsaidmanar@gmail.com>
2023-08-09 02:59:06 -0700

    adjust pycodestyle

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for a in range(ord('a'), ord('z') + 1):
     if a == ord('e') or a == ord('q'):
         continue
     else:
-        print("{:c}".format(a), end = "")
+        print("{:c}".format(a), end="")
