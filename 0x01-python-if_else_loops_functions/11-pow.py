0a35143d88e8a7483b51eb6794d1814638f4167e
ManarElsaid <elsaidmanar@gmail.com>
2023-08-10 02:13:01 -0700

    pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    for n in range(0, b - 1):
-        if b == 0:
-            a = 1
-        elif b < 0:
-            a = a / a
-    return a
+    return (a ** b)
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,4 @@ def print_last_digit(number):
     last_digit = number % 10
     if last_digit < 0:
         last_digit = number % (-10)
-    print("{:d}".format(last_digit), end="")
+    print(f"{last_digit}", end="")
