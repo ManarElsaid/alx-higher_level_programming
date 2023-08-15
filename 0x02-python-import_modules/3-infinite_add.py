142d26edf6865a89bb7df14b3aa57e6492177654
ManarElsaid <elsaidmanar@gmail.com>
2023-08-15 16:23:35 -0700

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """print the addition of all arguments"""
+    import sys
+
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+
+    print("{:d}".format(sum))
