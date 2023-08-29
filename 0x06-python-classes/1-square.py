a76593241b07da67c5e9c60162c8cc783d985a8d
ManarElsaid <elsaidmanar@gmail.com>
2023-08-29 12:46:48 -0700

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """class represents a square"""
+
+    def __init__(self, size):
+        """intialization the square
+
+        Arg:
+           size (int): the size of a square"""
+
+        self.__size = size
