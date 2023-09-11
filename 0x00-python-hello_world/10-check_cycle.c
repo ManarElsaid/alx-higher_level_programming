7590d49dcec64683fc32cdbe90716ce10f78515c
ManarElsaid <elsaidmanar@gmail.com>
2023-09-11 14:18:01 -0700

    remove last test case

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lists.h>
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: the singly linked list
+ *
+ * Return:0 if there is no cycle, 1 if there is a cycle
+ */
+
