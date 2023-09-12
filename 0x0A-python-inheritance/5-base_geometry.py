7131bba64751627faf9378a24522bae44749bdd2
ManarElsaid <elsaidmanar@gmail.com>
2023-09-11 19:06:56 -0700

    5-base_geometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""An empty class"""
+"""the module defines an empty class"""
 
 
 class BaseGeometry:
-    """This is an empty class"""
+    """defines an empty class"""
     pass
