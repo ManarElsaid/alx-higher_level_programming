fb77bec63f18caa9d2b9ecce1eb242c5de57dc7d
ManarElsaid <elsaidmanar@gmail.com>
2023-09-13 09:18:21 -0700

    1-write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module to write a string to a text file """
+
+
+def write_file(filename="", text=""):
+    """
+    this function a function that writes a string to a text file
+    (UTF8) and returns the number of characters written
+    """
+    with open(filename, mode= 'w', encoding= 'utf-8') as text_file:
+        return (text_file.write(text))
