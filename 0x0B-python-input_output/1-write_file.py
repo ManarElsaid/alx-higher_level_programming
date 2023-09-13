9945a443a834f719121e93fc3c12a5f5b1eddeb2
ManarElsaid <elsaidmanar@gmail.com>
2023-09-13 09:21:02 -0700

    pycodestyle wq1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
     this function a function that writes a string to a text file
     (UTF8) and returns the number of characters written
     """
-    with open(filename, mode= 'w', encoding= 'utf-8') as text_file:
+    with open(filename, mode='w', encoding='utf-8') as text_file:
         return (text_file.write(text))
