8007a82263b1bc5aee200b76a84b6df1fc37d374
ManarElsaid <elsaidmanar@gmail.com>
2023-09-13 19:28:42 -0700

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+"""
+a module contains class studen
+"""
+
+
+class Student:
+    """defines a student class"""
+    def __init__(self, first_name, last_name, age):
+        """initialize the object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        a method retrieves a dictionary representation
+        of a Student instance
+        """
+        return (self.__dict__)
