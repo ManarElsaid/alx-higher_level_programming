cd8b14895a027cf39b3881ace0fed342038392b2
ManarElsaid <elsaidmanar@gmail.com>
2023-09-19 12:02:18 -0700

    1, 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -36,3 +36,14 @@ class Rectangle:
         elif (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """public method returns a rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns a rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
