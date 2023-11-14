4c82c35c3d5c6b8132a767a86b742c8ff8be99c9
ManarElsaid <elsaidmanar@gmail.com>
2023-11-14 07:43:54 +0000

    1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let myVar = 'C is fun';
+let myVar = 'C is fun\n';
 console.log(myVar);
-myVar = 'Python is cool';
+myVar = 'Python is cool\n';
 console.log(myVar);
 myVar = 'JavaScript is amazing';
 console.log(myVar);
