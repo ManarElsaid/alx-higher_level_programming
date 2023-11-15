88a8ac7cb291863349dc2791817ef10254afbf36
ManarElsaid <elsaidmanar@gmail.com>
2023-11-15 07:45:16 +0000

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,4 +4,4 @@ const { argv } = require('process');
 
 if (process.argv.length === 2){ console.log('No argument'); }
 else if (process.argv.length === 3){ console.log('Argument found'); }
-else if (process.argv.length > 3 ) { console.log('Arguments found'); }
+else { console.log('Arguments found'); }
