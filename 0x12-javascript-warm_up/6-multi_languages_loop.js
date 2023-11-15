67dbf17d35383711cf07a4470e0db91ace550357
ManarElsaid <elsaidmanar@gmail.com>
2023-11-15 16:38:59 +0000

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,4 +2,4 @@
 
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while (i < 3) { console.log(myArray[i]); i++;}
+while (i < 3) { console.log(myArray[i]); i++; }
