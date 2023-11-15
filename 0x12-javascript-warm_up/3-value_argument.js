a75acd0f270a803d216cf6260484a860e5651468
ManarElsaid <elsaidmanar@gmail.com>
2023-11-15 08:01:28 +0000

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) { console.log('No argument'); } else if (process.argv.length === 3) { console.log(process.argv[2]); }
+if (process.argv[2] === undefined) { console.log('No argument'); } else { console.log(process.argv[2]); }
