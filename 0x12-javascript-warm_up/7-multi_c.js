d29c7ad36c32cbb44543e55cbe4b9a229526aca3
ManarElsaid <elsaidmanar@gmail.com>
2023-11-16 01:19:20 +0000

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const argc = Number(process.argv[2]);
-if (isNaN(argc)) { console.log('Missing number of occurrences'); } for(let i = 0; i < argc; i++) {console.log('C is fun'); }
+if (isNaN(argc)) { console.log('Missing number of occurrences'); } for (let i = 0; i < argc; i++) { console.log('C is fun'); }
