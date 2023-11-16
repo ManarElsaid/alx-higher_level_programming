cbafa62bc27325b96f8f6e54cf0a99eb809915c1
ManarElsaid <elsaidmanar@gmail.com>
2023-11-16 01:23:03 +0000

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const size = Number(process.argv[2]);
-if (isNaN(size)) { console.log('Missing size'); } for (let i = 0; i < size; i++) { console.log('x'.repeat(size)); }
+if (isNaN(size)) { console.log('Missing size'); } for (let i = 0; i < size; i++) { console.log('X'.repeat(size)); }
