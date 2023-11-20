dc8843f1cbb4bfb6dc4ed6bd8031353afdaf9a4c
ManarElsaid <elsaidmanar@gmail.com>
2023-11-19 18:06:31 -0800

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
             FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name LIKE BINARY %s ",{sys.argv[4]})
+            WHERE states.name LIKE BINARY %s ", {sys.argv[4]})
     print(", ".join([row[1] for row in cur.fetchall()]))
