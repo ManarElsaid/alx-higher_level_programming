eb73be9bc6b53dfdabfb048534b8b41391c5fa14
ManarElsaid <elsaidmanar@gmail.com>
2023-11-19 18:05:02 -0800

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name FROM \
-            cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name LIKE BINARY %s""", {sys.argv[4]})
-
+    cur.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
+            WHERE states.name LIKE BINARY %s ",{sys.argv[4]})
     print(", ".join([row[1] for row in cur.fetchall()]))
